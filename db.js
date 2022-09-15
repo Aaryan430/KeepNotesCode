b8fe0184519c11f9f9f5fd4813f9766234f25a54
Aaryan Choudhary <aaryan24092005@gmail.com>
2022-09-15 23:21:04 +0530

    fixws

--- db.js
+++ db.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config();
-const {JWT_SECRET,mongoURI} = require('./config/keys')
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, 
-        {
-            useNewUrlParser: "true",
-            useUnifiedTopology: "true"
-          
-          },
-        ()=>{
-        console.log("Connected to Mongo Successfully");
-    })
+const { JWT_SECRET, mongoURI } = require('./config/keys')
+const connectToMongo = async () => {
+    try {
+        mongoose.connect(mongoURI,
+            {
+                useNewUrlParser: "true",
+                useUnifiedTopology: "true"
+
+            },
+            () => {
+                console.log("Connected to Mongo Successfully");
+            })
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
 
 module.exports = connectToMongo;
